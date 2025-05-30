d613a077c25c01770763d9b7de25535c21cc7293
erikborsos <erikborsos21@gmail.com>
2025-05-30 18:59:54 +0200

    Changed link target if external

---
+++ src/components/markdown/LinkRenderer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let href: string;
+    export let text: string;
+
+    let blankTarget = !href.startsWith('/') && !href.startsWith('#') && !href.startsWith('mailto:') && !href.startsWith('tel:');
+</script>
+
+<a {href} target={blankTarget ? "_blank" : ""}>{text || href}</a>
--- src/components/markdown/Markdown.svelte
+++ src/components/markdown/Markdown.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     import SvelteMarkdown from "svelte-markdown";
     import CodeBlockRenderer from "./CodeBlockRenderer.svelte";
+    import LinkRenderer from "./LinkRenderer.svelte";
 
     let { content }: {content: string} = $props();
 </script>
 
 <div class="markdown">
-    <SvelteMarkdown source={content} renderers={{code: CodeBlockRenderer}}/>
+    <SvelteMarkdown source={content} renderers={{code: CodeBlockRenderer, link: LinkRenderer}}/>
 </div>
